51f47144abf71e9580ea30fd50821bf3b4187019
Danny Aziz <daanyaal997@gmail.com>
2022-01-02 19:03:46 +0000

    chore: setup

---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        primary: "#01030D",
+      },
+    },
+  },
+  plugins: [],
+};
