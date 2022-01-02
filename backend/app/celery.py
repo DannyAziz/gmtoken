c813ebced50658aaa5057e54d428a2a84482d310
Danny Aziz <daanyaal997@gmail.com>
2022-01-02 19:15:24 +0000

    feat: autoretry celery task

--- backend/app/celery.py
+++ backend/app/celery.py
@@ -17,7 +17,7 @@ celeryApp = Celery('tasks', broker=f'redis://{os.environ["REDIS_HOST"]}:6379/0')
 
 r = redis.Redis(host=os.environ["REDIS_HOST"], port=6379, db=0)
 
-@celeryApp.task
+@celeryApp.task(autoretry_for=(Exception,))
 def get_number_of_gms(address, username):
     count = 0
     for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'gm from:{username} since:2021-01-01 until:2021-12-31').get_items()):
